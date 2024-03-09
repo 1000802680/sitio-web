def3c492a2e889126c7ff7d500885ecbe2932877
1000802680 <162826576+1000802680@users.noreply.github.com>
2024-03-09 12:25:55 -0500

    Add files via upload

---
+++ pagina/menu.js
@@ -0,0 +1,22 @@
+const simbole =document.querySelector("simbole");
+const menu =document.querySelector(".menu-navegacion");
+
+
+
+console.log(menu)
+console.log(simbole)
+simbole.addEventListener('click ',()=>){
+}
+
+    function mostrarAlerta() {
+        // Mensaje de la alerta
+        var mensaje = "¡Esto es una advertencia!";
+
+        // Mostrar la alerta
+        alert(mensaje);
+    }
+
+    function menu-navegacion() {
+        var menu = document.getElementById('menu');
+        menu.style.display = (menu.style.display === 'none') ? 'block' : 'none';
+    }
